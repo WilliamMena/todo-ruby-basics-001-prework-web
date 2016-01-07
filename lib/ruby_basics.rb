deab01398ab92266331e555694665b3747d15a94
WilliamMena <williammena1991@gmail.com>
2016-01-07 01:42:10 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+    return 24/4 
 end
 
 def assign_variable(value)
-  
+    value
 end
 
-def argue
-  
+def argue(any)
+    any
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+    return "Nice"
 end
 
 def last_evaluated_value
-
+    return "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+    topping
 end
